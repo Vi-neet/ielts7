0a751f1a6855226d679a7cfc3c8786b5f97d6b09
Vineet Bhasin <bhasinvineet49@gmail.com>
2025-03-30 13:01:46 +0530

    added spacing in test questions

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,4 +1,5 @@
 import { useLocation, useNavigate } from "react-router-dom";
+import '../tests.css';
 // import { useState, useEffect } from "react";
 import QuestionSection from "@/components/QuestionSection";
 const TestPage = () => {
