eb868de77fb1bae69b6a4d8af9801f007b54f041
varunsaxena5elc@gmail.com <varunsaxena5elc@gmail.com>
2026-08-30 13:55:43 +0530

    fix: disable useWorkerdCondition in open-next.config.ts for jose compatibility

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,5 +2,8 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 
 const config = defineCloudflareConfig();
 config.default.minify = true;
+config.cloudflare = {
+  useWorkerdCondition: false,
+};
 
 export default config;
